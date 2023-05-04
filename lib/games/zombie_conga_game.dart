3ee8d9fdd07d3fb2c9c620c5436ce37686717b36
Stef-GMS <stephanie@GeekMeSpeak.com>
2023-05-03 20:26:22 -0700

    Add CatComponent and audio collision but sound doesn't play

--- lib/games/zombie_conga_game.dart
+++ lib/games/zombie_conga_game.dart
@@ -1,11 +1,16 @@
 import 'package:flame/game.dart';
+import 'package:flame_audio/flame_audio.dart';
+import 'package:zombie_conga_flame/components/cat_component.dart';
 //import 'package:flame/src/components/core/component.dart';
 //import '../components/background_component.dart';
 import '../components/zombie_component.dart';
+import '../constants/globals.dart';
 import '../inputs/joystick.dart';
 import '../components/background_parallax_component.dart';
 
-class ZombieCongaGame extends FlameGame {
+class ZombieCongaGame extends FlameGame with HasCollisionDetection {
+  int score = 0;
+
   @override
   Future<void> onLoad() async {
     await super.onLoad();
@@ -14,5 +19,12 @@ class ZombieCongaGame extends FlameGame {
     add(MyParallaxComponent());
     add(ZombieComponent(joystick: joystick));
     add(joystick);
+    add(CatComponent());
+
+    await FlameAudio.audioCache.loadAll(
+      [
+        Globals.hitCatSound,
+      ],
+    );
   }
 }
