2e53cae94b7647512ebb87a10225a322eacb9fa6
Stef-GMS <stephanie@GeekMeSpeak.com>
2025-03-18 18:16:35 -0700

    Changed score to cats

--- lib/loading/view/game_over_menu.dart
+++ lib/loading/view/game_over_menu.dart
@@ -37,7 +37,7 @@ class GameOverMenu extends StatelessWidget {
                 Padding(
                   padding: const EdgeInsets.symmetric(vertical: 50),
                   child: Text(
-                    'Score: ${gameRef.score}',
+                    'Score: ${gameRef.cats}',
                     style: const TextStyle(
                       fontSize: 50,
                     ),
